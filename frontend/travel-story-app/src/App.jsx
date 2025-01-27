c8360a04448942c8e7269c4ac9b6a3381a4e2fb5
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2025-01-27 09:56:27 +0700

    set up react-router-dom

--- frontend/travel-story-app/src/App.jsx
+++ frontend/travel-story-app/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" exact element={<Home/>} />
+          <Route path="/login" exact element={<Login/>} />
+          <Route path="/signup" exact element={<SignUp/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
