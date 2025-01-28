03a8a226fd4ae8f847a115832c00c419ecadac7a
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2025-01-28 15:48:37 +0700

    create account api

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,59 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
-
 const jwt = require("jsonwebtoken");
+
+const User = require("./models/user.model");
+
+mongoose.connect(config.connectionString);
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-// Test api
-app.get("/hello", async (req, res) => {
-  return res.status(200).json({
-    message: "Hello"
+// Create Account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if(!fullName || !email || !password){
+    return res.status(400).json({
+      error: true,
+      message: "All fields are required"
+    })
+  }
+
+  const isUser = await User.findOne({ email });
+  if(isUser){
+    return res.status(400).json({
+      error: true,
+      message: "User already exists"
+    })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    fullName, email, password: hashedPassword
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign(
+    { userId: user._id },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "72h"
+    }
+  )
+
+  return res.status(201).json({
+    error: false,
+    user: { fullName: user.fullName, email: user.email },
+    accessToken,
+    message: "Registration Successful"
   })
 });
 
